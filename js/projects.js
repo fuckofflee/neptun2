2086c06a9e0a4e2386c85ff57af113990d013a13
fuckofflee <lmartinbergot@gmail.com>
2026-02-27 11:42:01 +0100

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -21,10 +21,10 @@ export const ABOUT = {
 // Contact Page Content
 export const CONTACT = {
   en: {
-    title: 'mail:leonmartinbergot@hotmail.com\ntel:+33 6 52 08 05 88\nig:@neptunhuh\nPARIS\n\n\n\n\n\n\n\n\nlooking for an internship\ngetintouchwithmepls',
+    title: 'mail:leonmartinbergot@hotmail.com\ntel:+33 6 52 08 05 88\nig:@neptunhuh\nPARIS\n\n\n\n\n\n\nlooking for an internship\ngetintouchwithmepls',
   },
   fr: {
-    title: 'mail:leonmartinbergot@hotmail.com\ntel:+33 6 52 08 05 88\nig:@neptunhuh\nPARIS\n\n\n\n\n\n\n\n\nen recherche de stage\ncontactezmoisvp',
+    title: 'mail:leonmartinbergot@hotmail.com\ntel:+33 6 52 08 05 88\nig:@neptunhuh\nPARIS\n\n\n\n\n\n\nen recherche de stage\ncontactezmoisvp',
   },
   fontSize: '85px',
   fontFamily: 'TitleFont',
@@ -71,8 +71,8 @@ export const PROJECTS = [
       fr: "@stabilis\ncare design\nbricoler en\nsécurité\nprototypage\n2024-25"
     },
     description: {
-      en: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n@stabilis is a stabilizer for nails and screws. It was originally designed for a friend of mine whose hand was crushed in an escalator. After going through a long healing process, his hand sustained stabilization problems, specifically when DIYing. Stabilis' incorporated magnet and organic elongated shape allows the user to firmly maintain the screws and nails in position on any surface while protecting their precious hands.",
-      fr: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n@stabilis est un stabilisateur pour clous et vis. Il a été conçu à l'origine pour un ami dont la main a été broyée dans un escalator. Après un long processus de guérison, sa main a conservé des problèmes de stabilisation, en particulier lors du bricolage. La forme organique allongée de Stabilis et son aimant intégré permettent à l'utilisateur de maintenir fermement les vis et les clous en position sur n'importe quelle surface tout en protégeant ses mains."
+      en: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n@stabilis is a stabilizer for nails and screws. It was originally designed for a friend of mine whose hand was crushed in an escalator. After going through a long healing process, his hand sustained stabilization problems, specifically when DIYing. Stabilis' incorporated magnet and organic elongated shape allows the user to firmly maintain the screws and nails in position on any surface while protecting their precious hands.",
+      fr: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n@stabilis est un stabilisateur pour clous et vis. Il a été conçu à l'origine pour un ami dont la main a été broyée dans un escalator. Après un long processus de guérison, sa main a conservé des problèmes de stabilisation, en particulier lors du bricolage. La forme organique allongée de Stabilis et son aimant intégré permettent à l'utilisateur de maintenir fermement les vis et les clous en position sur n'importe quelle surface tout en protégeant ses mains."
     },
     mediaFiles: [
       'cover.jpg',
@@ -164,7 +164,7 @@ export const PROJECTS = [
     },
     description: {
       en: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n@lovesosa is a tribute website to Chief Keef. The user types in iconic Chief Keef related words to make the visuals appear. The user can then interact with the audioreactive visuals to rotate them around. The 3D visuals are loaded on a WEBGL page allowing for 3D objects display. Accessible at: https://fuckofflee.github.io/LOVESOSA/",
-      fr: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n@lovesosa est un site web hommage à Chief Keef. L'utilisateur tape des mots emblématiques liés à Chief Keef pour faire apparaître les visuels. Il peut ensuite interagir avec les objets pour les faire tourner sur eux-meme. Les visuels 3D sont chargés sur une page WEBGL permettant l'affichage d'objets 3D. Accessible sur : https://fuckofflee.github.io/LOVESOSA/"
+      fr: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n@lovesosa est un site web hommage à Chief Keef. L'utilisateur tape des mots emblématiques liés à Chief Keef pour faire apparaître les visuels. Il peut ensuite interagir avec les objets pour les faire tourner sur eux-meme. Les visuels 3D sont chargés sur une page WEBGL permettant l'affichage d'objets 3D. Accessible sur : https://fuckofflee.github.io/LOVESOSA/"
     },
     mediaFiles: [
       'cover2.mp4',
@@ -212,7 +212,7 @@ export const PROJECTS = [
     },
     description: {
       en: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n@sprayyy is a visual experiment critic on AI recognition. The goal was to crystallize the impact of AI recognition patterns on street art, most specifically on graffiti artists, often qualified as vandals. By using Mediapipe to recognize humans on the videos and modifying the output, the intent was to anonymize the artists again.",
-      fr: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n@sprayyy est une expérience visuelle critique de la reconnaissance IA. L'objectif était de cristalliser l'impact des modèles de reconnaissance IA sur l'art de rue, plus spécifiquement sur les graffeurs, souvent qualifiés de vandales. En utilisant Mediapipe pour reconnaître les humains dans les vidéos et en modifiant le résultat, l'intention était d'anonymiser à nouveau les artistes."
+      fr: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n@sprayyy est une expérience visuelle critique de la reconnaissance IA. L'objectif était de cristalliser l'impact des modèles de reconnaissance IA sur l'art de rue, plus spécifiquement sur les graffeurs, souvent qualifiés de vandales. En utilisant Mediapipe pour reconnaître les humains dans les vidéos et en modifiant le résultat, l'intention était d'anonymiser à nouveau les artistes."
     },
     mediaFiles: [
       'image1.mp4',
@@ -236,7 +236,7 @@ export const PROJECTS = [
     },
     description: {
       en: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nIn my second year, I did a 3 months internship at a french furniture publishing house called SOLLEN Design, based in Bordeaux. During this period, I had the chance to work on multiple projects, such as rethinking the main visuals for their website, designing new pieces of furniture to complete an already existing collection, as well as designing and supervising the production of a brand new furniture piece. It was a truly enriching experience despite the company putting another designer's name on my chair design.",
-      fr: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nDurant ma deuxième année, j'ai effectué un stage de 3 mois dans une maison d'édition de mobilier française appelée SOLLEN Design, basée à Bordeaux. Pendant cette période, j'ai eu la chance de travailler sur plusieurs projets, tels que repenser les principaux visuels de leur site web, concevoir de nouvelles pièces de mobilier pour compléter une collection déjà existante, ainsi que concevoir et superviser la production d'une toute nouvelle pièce de mobilier. Ce fut une expérience vraiment enrichissante malgré le fait que l'entreprise ait mis le nom d'un autre designer sur mon design de chaise."
+      fr: "\n\n\n\n\n\n\n\n\n\n\n\n\n\nDurant ma deuxième année, j'ai effectué un stage de 3 mois dans une maison d'édition de mobilier française appelée SOLLEN Design, basée à Bordeaux. Pendant cette période, j'ai eu la chance de travailler sur plusieurs projets, tels que repenser les principaux visuels de leur site web, concevoir de nouvelles pièces de mobilier pour compléter une collection déjà existante, ainsi que concevoir et superviser la production d'une toute nouvelle pièce de mobilier. Ce fut une expérience vraiment enrichissante malgré le fait que l'entreprise ait mis le nom d'un autre designer sur mon design de chaise."
     },
     mediaFiles: [
       'cover.jpg',
@@ -323,7 +323,7 @@ export const PROJECTS = [
     },
     description: {
       en: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n@live.pixels is a website made to display creative coding experiments. All the original images are prints of the first Sino-Japanese war (1894-1895) sourced through the national french digital library (Gallica). They were then modified with code to create interesting visual patterns over the image or directly on the image. Accessible at: https://fuckofflee.github.io/LIvE.PiXeLS/",
-      fr: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n@live.pixels est un site web créé pour afficher mes expérimentations de codage créatif. Toutes les images originales sont des estampes de la première guerre sino-japonaise (1894-1895) provenant de la bibliothèque numérique nationale française (Gallica). Elles ont ensuite été modifiées avec du code pour créer des motifs visuels intéressants sur l'image ou directement dans l'image. Accessible sur : https://fuckofflee.github.io/LIvE.PiXeLS/"
+      fr: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n@live.pixels est un site web créé pour afficher mes expérimentations de codage créatif. Toutes les images originales sont des estampes de la première guerre sino-japonaise (1894-1895) provenant de la bibliothèque numérique nationale française (Gallica). Elles ont ensuite été modifiées avec du code pour créer des motifs visuels intéressants sur l'image ou directement dans l'image. Accessible sur : https://fuckofflee.github.io/LIvE.PiXeLS/"
     },
     mediaFiles: [
       'cover2.mp4',
@@ -342,7 +342,7 @@ export const PROJECTS = [
     },
     description: {
       en: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n@bare system is a sound system designed to amplify sound saturation. It was made using parts of a broken surround sound system picked up on the streets of Paris. Its empty structure is made entirely of metal sheets and screws to modify the sound's resonance and saturation by vibrating through the metal. Professional visuals are underway.",
-      fr: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n@bare system est un système son conçu pour amplifier la saturation sonore. Il a été fabriqué à partir de pièces d'un système son surround cassé ramassé dans les rues de Paris. Sa structure vide est entièrement faite de tôles et de vis pour modifier la résonance et la saturation du son en vibrant à travers le métal. Des visuels professionnels sont en cours de développement."
+      fr: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n@bare system est un système son conçu pour amplifier la saturation sonore. Il a été fabriqué à partir de pièces d'un système son surround cassé ramassé dans les rues de Paris. Sa structure vide est entièrement faite de tôles et de vis pour modifier la résonance et la saturation du son en vibrant à travers le métal. Des visuels professionnels sont en cours de développement."
     },
     mediaFiles: [
       'cover2.jpg',
@@ -400,7 +400,7 @@ export const PROJECTS = [
     },
     description: {
       en: "\n\n\n\n\n\n\n\n\n\n@telly is a 3D visual displayer for 2D projects. It instances 3D objects, displayed using POPs, and fetches project visuals inside of a folder to display them on the TV screens. A MIDI mapping system was also implemented to control the rotation aspect of the TVs. The sound design was made using recordings of a classmate's voice which were then modified on Ableton to match the visual ambiance of the television visuals.",
-      fr: "\n\n\n\n\n\n\n\n@telly est un système d'affichage visuel 3D pour projets 2D. Il instancie des objets 3D, affichés via les POPs, et récupère les visuels de projet dans un dossier pour les afficher sur les écrans de télévision. Un système de mapping MIDI a également été implémenté pour contrôler l'aspect rotation des télévisions. Le sound design a été réalisé à partir d'enregistrements de la voix d'un camarade de classe qui ont ensuite été modifiés sur Ableton pour correspondre à l'ambiance visuelle des téléviseurs."
+      fr: "\n\n\n\n\n\n@telly est un système d'affichage visuel 3D pour projets 2D. Il instancie des objets 3D, affichés via les POPs, et récupère les visuels de projet dans un dossier pour les afficher sur les écrans de télévision. Un système de mapping MIDI a également été implémenté pour contrôler l'aspect rotation des télévisions. Le sound design a été réalisé à partir d'enregistrements de la voix d'un camarade de classe qui ont ensuite été modifiés sur Ableton pour correspondre à l'ambiance visuelle des téléviseurs."
     },
     mediaFiles: [
       'image1.mp4',
